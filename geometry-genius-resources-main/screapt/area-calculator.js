5d9a834ddfc730f31b5902de3c0aaf4770624731
brainiac1010 <armanmahim00@gmail.com>
2024-06-18 14:04:18 +0600

    some foxxes add

--- geometry-genius-resources-main/screapt/area-calculator.js
+++ geometry-genius-resources-main/screapt/area-calculator.js
@@ -10,6 +10,7 @@ function calculateTriangleArea() {
 
     const area = 0.5 * base * height;
     setElementInnerText('display-area', area);
+    addToCalculationEnty('Triangle', area);
 }
 
 // Function to calculate the area of a rectangle
@@ -24,6 +25,7 @@ function calculateRectangleArea() {
 
     const area = width * length;
     setElementInnerText('display-rectangle-area', area);
+    addToCalculationEnty('Rectangle', area);
 }
 
 // Function to calculate the area of a parallelogram
@@ -38,6 +40,7 @@ function calculateParallelogramArea() {
 
     const area = base * height;
     setElementInnerText('display-parallelogram-area', area);
+    addToCalculationEnty('Parallelogram', area);
 }
 
 // Function to calculate the area of an ellipse
@@ -52,6 +55,7 @@ function calculateEllipseArea() {
 
     const area = Math.PI * majorRadius * minorRadius;
     setElementInnerText('display-ellipse-area', area.toFixed(2));
+    addToCalculationEnty('Ellipse', area.toFixed(2));
 }
 
 // Function to calculate the area of a rhombus
@@ -66,6 +70,7 @@ function calculateRhombusArea() {
 
     const area = 0.5 * d1 * d2;
     setElementInnerText('display-rhombus-area', area);
+    addToCalculationEnty('Rhombus', area);
 }
 
 // Function to calculate the area of a pentagon
@@ -80,6 +85,7 @@ function calculatePentagonArea() {
 
     const area = 0.5 * perimeter * apothem;
     setElementInnerText('display-pentagon-area', area);
+    addToCalculationEnty('Pentagon', area);
 }
 
 // Reusable function to get input field value as a number
@@ -95,9 +101,21 @@ function setElementInnerText(elementId, value) {
     textField.innerText = value;
 }
 
+// Add to calculation entry
+/**
+ 1. Get the element where you want to add the dynamic HTML.
+ 2. Create an element you want to add.
+ 3. If needed, add some class.
+ 4. Set inner HTML (it could be a dynamic template string).
+ 5. Append the created element as a child of the parent.
+ */
+function addToCalculationEnty(areaType, area) {
+    
+    const calculationEntry = document.getElementById('calculation-entry');
 
-//add to calculation entry
-function addToCalculationEnty(){
-
-    console.log("adding soon ")
+    const count = calculationEntry.childElementCount;
+    const p = document.createElement('p');
+    p.classList.add('my-2');
+    p.innerHTML = `${count + 1} . ${areaType} ${area} cm <sup>2</sup> <button class= "bg-blue-300 px-4 rounded-md font-bold mt-4">Convert</button>`;
+    calculationEntry.appendChild(p);
 }
